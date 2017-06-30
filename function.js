c279cfbf918b9cf19dab39c8bde7df2acd2e94ca
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-06-30 16:17:07 -0500

    change color

--- function.js
+++ function.js
@@ -4,39 +4,38 @@ $(document).ready(function(){
     $('span').on('mouseleave', removeHighlight);
 
 
-
-
 })
 var mathArray = [];
 
 function displayChar(){
     var chara = $(this).text();
     var id = $(this).attr('id')
-    if($.isNumeric(chara) || chara == "."){
-        $('#readout').append(chara);
-        mathArray.push(chara);
-        console.log('numeric');
-        console.log(chara)
-    } 
-    else if (chara == "c") {
-        $('#readout').empty();
-        mathArray.length = 0;
-    } 
-    else if (id == "result"){
-        compute();
-        console.log('am end');
-    }
-    else if (id == 'multiply', 'divide', 'add', 'subtract'){
-        $('#readout').text(chara);
-        mathArray.push(chara)
-        console.log(id);
-    } 
-    else {
-        console.log('not a num');
-    }
-    console.log(mathArray);
-}
-var operators = ['+', '-', '*', '/'];
+        if ($.isNumeric(chara) || chara == "."){
+            $('#readout').append(chara);
+            mathArray.push(chara);
+            console.log('numeric');
+            console.log(chara)
+        } 
+        else if (chara == "c") {
+            $('#readout').empty();
+            mathArray.length = 0;
+        } 
+        else if (id == "result"){
+            compute();
+            console.log('am end');
+        }
+        else if (id == 'multiply', 'divide', 'add', 'subtract'){
+            $('#readout').text(chara);
+            mathArray.push(chara)
+            console.log(id);
+        } 
+        else {
+            console.log('not a num');
+        }
+        console.log(mathArray);
+ }
+
+// var operators = ['+', '-', '*', '/'];
 
 //function for =, runs math (which is in array?) then displays onj #readout
 
